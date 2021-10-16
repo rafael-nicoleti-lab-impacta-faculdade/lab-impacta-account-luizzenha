ee45dea7aec86d349d07cbe675a12bc2e2b1d666
Luiz Zenha <lzenha@gmail.com>
2021-10-16 13:05:31 -0300

    Test cases

--- account/src/test/java/br/com/lab/impacta/account/model/AccountTest.kt
+++ account/src/test/java/br/com/lab/impacta/account/model/AccountTest.kt
@@ -1,4 +1,23 @@
 package br.com.lab.impacta.account.model
 
-class AccountTest {
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.AfterAll
+import kotlin.test.assertFalse
+
+
+internal class AccountTest {
+
+    private var conta: Account = Account(1,123,100.0,Person(1,"joao","123455433"))
+
+
+
+    @Test
+    fun testar_debit_true() {
+        assert(conta.debit(10.0))
+    }
+
+    @Test
+    fun testar_debit_false() {
+        assertFalse(conta.debit(120.0))
+    }
 }
